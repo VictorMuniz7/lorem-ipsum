64d99533e95acf7c0ac88bf1a27e3b2b3cfee14a
Kairuus <kingkairuus@gmail.com>
2023-07-28 14:48:29 -0300

    Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoremIpsum } from "lorem-ipsum";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lorem-ipsum';
+
+  lorem = new LoremIpsum();
+
+  text: string = ''
+  paragraphs = 0
+  copyText = 'Copy Lorem Ipsum'
+
+  generateLoremIpsum(){
+
+    if(this.paragraphs > 30){
+      this.paragraphs = 30
+    }
+    if(this.paragraphs <= 0){
+      this.paragraphs = 0
+    }
+
+    this.text = this.lorem.generateParagraphs(this.paragraphs);
+  }
+
+  copyLorem(){
+    this.copyText = 'Copied!'
+    navigator.clipboard.writeText(this.text)
+
+    setTimeout(() => {
+      this.copyText = 'Copy Lorem Ipsum'
+    }, 5000)
+  }
+
 }
